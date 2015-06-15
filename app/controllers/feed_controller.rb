984aeacfc7b8a44f1000a6eebf4ba36282382ad0
shunsuke227ono <shunsuke227ono@gmail.com>
2015-06-15 18:52:07 +0930

    fix bug of similarity.rake and add view

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,10 +1,12 @@
 class FeedController < ApplicationController
-  before_action :set_url
-  def index
-    @feed = Feedjira::Feed.fetch_and_parse @url
-  end
-  private
-  def set_url
-    @url = Settings.rss.livedoor.full.try(params[:category])
+  def index(category="top", page=1, realtime=false)
+    if realtime
+      url = Settings.rss.livedoor.full.try(params[:category])
+      @feed = Feedjira::Feed.fetch_and_parse @url
+    else
+      category
+      category_id = Article.categories[category.to_sym]
+      @feed = Article.where(category: category_id).page(page).per(12)
+    end
   end
 end
