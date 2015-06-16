dab374c66b838b997210bf8d818867b0b40184b4
shunsuke227ono <shunsuke227ono@gmail.com>
2015-06-16 17:16:57 +0930

    add method to get img

--- lib/rss/full_content.rb
+++ lib/rss/full_content.rb
@@ -3,9 +3,17 @@ module FullContent
   def self.article_body(link, resource_name)
     case resource_name
     when :livedoor
-      page = Nokogiri::HTML(open(link)).at("[@itemprop='articleBody']").inner_text
+      Nokogiri::HTML(open(link)).at("[@itemprop='articleBody']").inner_text
     when :yahoo
-      page = Nokogiri::HTML(open(link)).css("p.ynDetailText").inner_text
+      Nokogiri::HTML(open(link)).css("p.ynDetailText").inner_text
     end
+  end
+  def self.img_and_article_body(link)
+    # livedoorのみ
+    res = {}
+    page = Nokogiri::HTML(open(link))
+    res[:article_body] = page.at("[@itemprop='articleBody']").inner_text
+    res[:img] = page.at("[@itemprop='image']").attr('src')
+    res
   end
 end
