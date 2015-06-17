92f50520146f1b755a23f7736b2358e8d5c8ad91
shunsuke227ono <shunsuke227ono@gmail.com>
2015-06-18 02:03:19 +0930

    adjust the length of article and a underline

--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -8,11 +8,15 @@ class ArticleDecorator < Draper::Decorator
     return object.content.truncate(number_of_words(160))
   end
   private
+  def very_long_title?
+    object.title.size > 35
+  end
   def long_title?
     object.title.size > 25
   end
   def number_of_words(default_number)
-    default_number * 3/4 if long_title?
+    return default_number * 1/2 if very_long_title?
+    return default_number * 3/4 if long_title?
     default_number
   end
 end
